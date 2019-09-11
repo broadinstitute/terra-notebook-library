868165895e522332e2fc910ed1497ff6cbb64a36
Morgan Taylor <marymorg@broadinstitute.org>
2019-09-11 13:57:40 -0400

    removed function in gatk_setup

--- gatk_setup.py
+++ gatk_setup.py
@@ -1,7 +1,6 @@
 """ setup files for GATKTutorials-Germline notebooks
 """
 
-def gatk_setup():
-    # Set your workspace bucket variable for this notebook.
-    import os
-    BUCKET = os.environ['WORKSPACE_BUCKET']
+# Set your workspace bucket variable for this notebook.
+import os
+BUCKET = os.environ['WORKSPACE_BUCKET']
