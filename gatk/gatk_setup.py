05ee48198ea2882a8275c15a51d842ec1fcf844b
M. Morgan Taylor <marymorg@broadinstitute.org>
2019-09-12 11:21:05 -0400

    Update gatk_setup.py

--- gatk/gatk_setup.py
+++ gatk/gatk_setup.py
@@ -4,3 +4,13 @@
 # Set your workspace bucket variable for this notebook.
 import os
 BUCKET = os.environ['WORKSPACE_BUCKET']
+
+# Set workshop variable to access the most recent materials
+WORKSHOP = "workshop_1908"
+
+# Create directories for your files to live inside this notebook
+! mkdir -p /home/jupyter-user/2-germline-vd/sandbox/
+! mkdir -p /home/jupyter-user/2-germline-vd/ref
+! mkdir -p /home/jupyter-user/2-germline-vd/resources
+! mkdir -p /home/jupyter-user/2-germline-vd/gvcfs
+! mkdir -p /home/jupyter-user/CNN/Output/
