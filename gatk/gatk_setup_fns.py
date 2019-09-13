ab781a128fd4ba835fbb835c3c926aa35575c692
Morgan Taylor <marymorg@broadinstitute.org>
2019-09-13 13:37:58 -0400

    updated gatk_setup_fns

--- gatk/gatk_setup_fns.py
+++ gatk/gatk_setup_fns.py
@@ -17,16 +17,16 @@ def gatk_setup(verbose):
                     "/home/jupyter-user/CNN/Output/"]
 
     for path in dirs_to_create:
-    if not os.path.exists(path):
-        os.makedirs(path)
+        if not os.path.exists(path):
+            os.makedirs(path)
 
     # Check if data is accessible. The command should list several gs:// URLs.
     system_command = "gsutil ls gs://gatk-tutorials/"+WORKSHOP+"/2-germline/"
     file_contents = os.popen(system_command).read()
     if verbose:
-    print("\n\nChecking if data is accessible. This should list several gs:// URLS:")
-    for f in file_contents.split('\n'):
-        print(f)
+        print("\n\nChecking if data is accessible. This should list several gs:// URLS:")
+        for f in file_contents.split('\n'):
+            print(f)
 
     # Download Data to the Notebook
     system_commands = ["gsutil cp gs://gatk-tutorials/"+WORKSHOP+"/2-germline/ref/* /home/jupyter-user/2-germline-vd/ref",
@@ -35,9 +35,9 @@ def gatk_setup(verbose):
                     "gsutil cp gs://gatk-tutorials/"+WORKSHOP+"/2-germline/gvcfs/* /home/jupyter-user/2-germline-vd/gvcfs/"]
 
     for system_command in system_commands:
-    os.system(system_command)
-    target_folder = system_command.split(" ")[-1]
-    copied_files = os.popen("ls "+target_folder).read()
-    if verbose:
-        print("Copied files to "+target_folder+":")
-        print(copied_files)
+        os.system(system_command)
+        target_folder = system_command.split(" ")[-1]
+        copied_files = os.popen("ls "+target_folder).read()
+        if verbose:
+            print("Copied files to "+target_folder+":")
+            print(copied_files)
