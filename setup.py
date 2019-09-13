a7ee92b4f7170bd1a8faa0e6b0dcde998e40b5df
Morgan Taylor <marymorg@broadinstitute.org>
2019-09-13 09:46:29 -0400

    created setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='terranblib',
+      version='0.1',
+      description='Library of code for Terra Notebooks',
+      url='https://github.com/broadinstitute/terranblib',
+      author='DSP Field Engineering',
+      author_email='marymorg@broadinstitute.org',
+      license='MIT',
+      packages=['gatk'],
+      zip_safe=False)
