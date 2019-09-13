763a751224737a913457089e3e4e20a57ef2eaa8
Morgan Taylor <marymorg@broadinstitute.org>
2019-09-13 16:04:13 -0400

    revert changes

--- gatk/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Set your workspace bucket variable for this notebook.
-BUCKET = os.environ['WORKSPACE_BUCKET']
-
-# Set workshop variable to access the most recent materials
-WORKSHOP = "workshop_1908"
--- gatk/gatk_setup_fns.py
+++ gatk/gatk_setup_fns.py
@@ -2,6 +2,12 @@
 """
 import os
 
+# Set your workspace bucket variable for this notebook.
+BUCKET = os.environ['WORKSPACE_BUCKET']
+
+# Set workshop variable to access the most recent materials
+WORKSHOP = "workshop_1908"
+
 def gatk_setup_1(verbose=False):
     # Create directories for your files to live inside this notebook
     dirs_to_create = ["/home/jupyter-user/2-germline-vd/sandbox/",
