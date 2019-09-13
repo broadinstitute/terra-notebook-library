0f282ca02b774c58cc28b60b094cb1db73af0c7d
Morgan Taylor <marymorg@broadinstitute.org>
2019-09-13 16:43:57 -0400

    abandoned global variables for now

--- gatk/gatk_setup_fns.py
+++ gatk/gatk_setup_fns.py
@@ -3,15 +3,13 @@
 import os
 
 
-# # Set your workspace bucket variable for this notebook.
-# BUCKET = os.environ['WORKSPACE_BUCKET']
+# Set your workspace bucket variable for this notebook.
+BUCKET = os.environ['WORKSPACE_BUCKET']
 
-# # Set workshop variable to access the most recent materials
-# WORKSHOP = "workshop_1908"
+# Set workshop variable to access the most recent materials
+WORKSHOP = "workshop_1908"
 
 def gatk_init(verbose=False):
-    globals()["BUCKET"] = os.environ['WORKSPACE_BUCKET']
-    globals()["WORKSHOP"] = "workshop_1908"
 
     # Create directories for your files to live inside this notebook
     dirs_to_create = ["/home/jupyter-user/2-germline-vd/sandbox/",
